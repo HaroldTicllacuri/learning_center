0e580b273be4a4945ac09681b3a1bfb3bd92378a
HaroldTicllacuri <u202214214@upc.edu.pe>
2024-09-30 22:44:10 -0500

    feat(main-view)

--- src/router/index.js
+++ src/router/index.js
@@ -4,9 +4,15 @@
  * @docs: https://router.vuejs.org/
  */
 import {createRouter, createWebHistory} from "vue-router";
+import HomeComponent from "../public/pages/home.component.vue";
+import AboutComponent from "../public/pages/about.component.vue";
 const router = createRouter({
   history: createWebHistory(),
-  routes: [],
+  routes: [
+    { path: '/home', component: HomeComponent, meta: { title: 'Home' } },
+    { path: '/about', component: AboutComponent, meta: { title: 'About us' } },
+    { path: '/', redirect: '/home' },
+  ],
 });
 /**
  * Set Business name as prefix for each page title
