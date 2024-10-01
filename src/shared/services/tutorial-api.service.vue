ccba8be968b3e34c4a1ab9e6dde027b8aebc777f
HaroldTicllacuri <u202214214@upc.edu.pe>
2024-09-30 23:04:05 -0500

    feat(tutorials)

---
+++ src/shared/services/tutorial-api.service.vue
@@ -0,0 +1,31 @@
+import http from "../../shared/services/http-common.js";
+/**
+* TutorialsApiService class
+* @description Service class for making HTTP requests to the API
+*/
+export class TutorialsApiService {
+
+getAll() {
+return http.get('/tutorials');
+}
+
+  getById(id) {
+  return http.get(`/tutorials/${id}`);
+  }
+
+    create(tutorialResource) {
+    return http.post('/tutorials', tutorialResource);
+    }
+
+      update(id, tutorialResource) {
+      return http.put(`/tutorials/${id}`, tutorialResource);
+      }
+
+        delete(id) {
+        return http.delete(`/tutorials/${id}`);
+        }
+
+          findByTitle(title) {
+          return http.get(`/tutorials?title=${title}`);
+          }
+}
